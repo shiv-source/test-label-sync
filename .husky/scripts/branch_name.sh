a9d0fd1c737cfc55408119ec1d07e34c2c971814
SHIV KUMAR <shivk7466@gmail.com>
2023-10-22 01:50:15 +0530

    script fixed

--- .husky/scripts/branch_name.sh
+++ .husky/scripts/branch_name.sh
@@ -1,58 +1,36 @@
 #!/usr/bin/env sh
 
-# BRANCHE_NAME_STARTS_WITH=('feature' 'bugfix' 'hotfix' 'test' 'docs' 'release')  #Add more branch name prefix here 
+# Valid branch will be something like; feature/some_feature_name , bugfix/some_bugfix_name , 
+# please only update valid_branches list 
+valid_branches=('feature' 'bugfix' 'hotfix' 'test' 'docs' 'release')
 
+
+
+is_passed=false
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-# for val in "${PROTECTED_BRANCHES[@]}"
-# do
-#    if [ "$branch" = "$val" ]; then
-#         echo "You can't commit directly to $val branch"
-#         exit 1
-#     fi
-# done
-
-
-# if [[ $branch =~ ^feature/.* ]]; then
-#   echo "✅ The branch starts with $branch is a valid branches. ✅"
-# else
-#   echo "❌ The is an invalid branche."
-# fi
-
-
-# valid_branches=("feature/" "bugfix/" "hotfix/")
-
-# for branch in "${valid_branches[@]}"; do
-#     echo $branch 
-#     echo $current_branch
-#   if [[ $current_branch =~ ^$branch$ ]]; then
-#     echo ""
-#     echo "✅ The branch starts with $branch is a valid branch ✅"
-#     echo ""
-#   else
-#     echo ""
-#     echo "❌ You current branch $current_branch is an invalid branche. ❌"
-#     echo ""
-#     echo "💁 Suggestions: Branch name should be like "$branch"some_text"
-#     echo ""
-
-#     for val in "${valid_branches[@]}"; do
-#         echo "  ➡️  The branch can be start only with $val and should be like this "$val"some_text "
-#         echo ""
-#     done
-#     echo ""
-#     echo ""
-#     exit 1
-#   fi
-# done
-
-
-
-valid_branches=("feature/" "bugfix/" "hotfix/")
-
 for branch in "${valid_branches[@]}"; do
-  if [[ $branch =~ ^$branch$ ]]; then
-    echo "The branch starts with $branch is a valid branch"
-    break
-  fi
+    regex=^$branch/.*   #^bugfix/.*
+    if [[ $current_branch =~ $regex ]]; then
+        is_passed=true
+        echo ""
+        echo "✅ The branch starts with $current_branch is a valid branch ✅"
+        echo ""
+        break
+    fi
 done
+
+if [[ $is_passed == "false" ]]; then
+    echo ""
+    echo "❌ You current branch $current_branch is an invalid branche. ❌"
+    echo ""
+    echo "💁 Suggestions: Branch name should be like "$branch"some_text"
+    echo ""
+    for val in "${valid_branches[@]}"; do
+        echo "  ➡️  The branch can be start only with $val and should be like this "$val"some_text "
+        echo ""
+    done
+    echo ""
+    echo ""
+    exit 1
+fi
